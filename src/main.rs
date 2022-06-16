16ddb2e0840ac8fe66dd6f533f27a51a58822e32
GhosTHaise <Ghostrex2@gmail.com>
2022-06-16 21:27:34 +0300

    Retrieve header from url and send response

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::io::prelude::*;
+
 fn main() {
-    let listener:TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener:TcpListener = TcpListener::bind("127.0.0.1:7879").unwrap();
     
     for stream in listener.incoming(){
         let stream:TcpStream = stream.unwrap();
-        println!("Connetion etablished !");
+        
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream:TcpStream){
+    let mut buffer: [u8;1024] = [0;1024];
+
+    stream.read(&mut buffer).unwrap();
+    /* header
+    println!(
+        "Request : {}",
+        String::from_utf8_lossy(&buffer[..])); */
+    let response : &str = "HTTP/1.1 200 OK/r/n/r/n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
