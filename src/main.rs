ee7da042d23172455c40049ceeda17a04601ebac
GhosTHaise <Ghostrex2@gmail.com>
2022-06-17 11:08:21 +0300

    Amelioration de syntaxe avec destructuration

--- src/main.rs
+++ src/main.rs
@@ -22,28 +22,20 @@ fn handle_connection(mut stream:TcpStream){
         String::from_utf8_lossy(&buffer[..])); */
     let get : &[u8;16] = b"GET / HTTP/1.1\r\n"; 
     
-    if buffer.starts_with(get){
-        let contents : String = fs::read_to_string("template/index.html").unwrap();
-        let response : String = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                        contents.len(),
-                        contents);
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }else{
-        let status_line:&str = "HTTP/1.1 404 NOT FOUND";
-
-        let contents: String = fs::read_to_string("template/Error.html").unwrap();
-        let response : String = format!("{}\r\nContent-lentgh : {}\r\n\r\n{}",
+    let (status_line , filename) = 
+            if buffer.starts_with(get) {
+                ("HTTP/1.1 200 OK","index.html")
+            }else{
+                ("HTTP/1.1 404 NOT FOUND","Error.html")
+            };
+    let contents: String = fs::read_to_string(format!("template/{}",filename)).unwrap();
+    let response : String = format!("{}\r\nContent-lentgh : {}\r\n\r\n{}",
                 status_line,
                 contents.len(),
                 contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-        let responseView:String =  format!("Response en bytes : {}",response);
-        println!("{:?}",responseView.as_bytes());
-
-    }
-    
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    let responseView:String =  format!("Response en bytes : {}",response);
+    println!("{:?}",responseView.as_bytes());
     
 }
