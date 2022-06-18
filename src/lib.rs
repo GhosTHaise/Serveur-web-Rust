d5ee436779a05ab357f341985086bd949f26f900
GhosTHaise <Ghostrex2@gmail.com>
2022-06-18 22:24:53 +0300

    Implement thread method

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-pub struct ThreadPool;
+use std::thread;
+pub struct ThreadPool{
+    threads : Vec<thread::JoinHandle<()>>
+}
 
 impl ThreadPool {
+    //Creer un ThreadPool
+    //
+    //Size : nombre de thread
+    //
+    //la nouvel fonction Panic si thread < 0
+    //
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+        let mut threads = Vec::with_capacity(size);
+        for _ in 0..size {
+
+        }
+        ThreadPool {threads}
     }
+
+    pub fn execute<F>(&self,f: F)
+        where
+            F: FnOnce() + Send + 'static
+                {
+
+                }
 }
