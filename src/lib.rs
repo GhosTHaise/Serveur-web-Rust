5845c252d04f2358bdba3a46bedfc40d73df0a95
GhosTHaise <Ghostrex2@gmail.com>
2022-06-20 21:36:21 +0300

    Pass informations between threads

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub struct ThreadPool{
     workers : Vec<Worker>,
     sender : mpsc::Sender<Job>
 }
-struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 impl ThreadPool {
     //Creer un ThreadPool
     //
@@ -31,7 +31,8 @@ impl ThreadPool {
         where
             F: FnOnce() + Send + 'static
                 {
-
+                    let job = Box::new(f);
+                    self.sender.send(job).unwrap();
                 }
 }
 struct Worker {
@@ -41,7 +42,12 @@ struct Worker {
 
 impl Worker {
     fn new(id : usize,receiver:Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(||{});
+        let thread = thread::spawn(move || loop{
+             let job = receiver.lock().unwrap().recv().unwrap();
+             println!("Worker {} got a job ; executing ...",id);
+        });
+       
+
         Worker{id,thread}
     }
 }
