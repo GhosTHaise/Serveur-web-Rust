87e2e3557fbf8a478d47902ccfe2604c049d5347
GhosTHaise <Ghostrex2@gmail.com>
2022-06-21 21:41:15 +0300

    Add drop implementation

--- src/lib.rs
+++ src/lib.rs
@@ -35,9 +35,19 @@ impl ThreadPool {
                     self.sender.send(job).unwrap();
                 }
 }
+impl Drop for ThreadPool{
+    fn drop(&mut self){
+        for worker in &mut self.workers{
+            println!("Shutting down worker {}",worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
 struct Worker {
     id : usize,
-    thread : thread::JoinHandle<()>
+    thread : Option<thread::JoinHandle<()>>
 }
 
 impl Worker {
@@ -50,6 +60,6 @@ impl Worker {
         });
        
 
-        Worker{id,thread}
+        Worker{id,thread: Some(thread)}
     }
 }
